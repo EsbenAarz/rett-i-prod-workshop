af6fd1f207eae35dafc53887a0672f4c7d06b541
Esben Aarseth <esben.aarseth@gmail.com>
2020-09-15 15:57:46 +0200

    Fix

--- src/controller/super-important-service.js
+++ src/controller/super-important-service.js
@@ -1,6 +1,7 @@
 export function superImportantService() {
     return {
         "hello": "I AM ALIVE",
-        "to": "production and beyond"
+        "to": "production and beyond",
+        "did-the-deploy": "work?"
     };
 }
