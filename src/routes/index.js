950f1c860f6e82b8070871ffc6739fd3762fff24
Esben Aarseth <esben.aarseth@gmail.com>
2020-09-30 15:31:59 +0200

    Adding prometheus-setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { superImportantService } from "../controller/super-important-service";
+import { superImportantService, userService } from "../controller/super-important-service";
 
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.send(superImportantService())
+    res.send(superImportantService());
+});
+
+router.get('/users', function(req, res, next) {
+    res.status(200);
+    res.send(userService());
 });
 
 export default router
