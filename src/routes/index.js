e19da367e2163494ad5f5fc93038296419c1b339
Esben Aarseth <esben.aarseth@gmail.com>
2020-10-29 11:28:52 +0100

    Removing timeout

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,14 +5,15 @@ const router = express.Router();
 
 router.get('/', function(req, res) {
     const random = Math.random() * 1000;
-    if (random >= 300 && random <= 400) {
+    /*if (random >= 300 && random <= 400) {
         res.status(429);
         res.send(JSON.stringify({"error": "This is bad"}));
     } else {
         setTimeout(() => {
             res.send(superImportantService());
         }, random);
-    }
+    }*/
+    res.send(superImportantService());
 });
 
 router.get('/users', function(req, res) {
